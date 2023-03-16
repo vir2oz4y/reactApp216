941162d30587c9166ebab88b04d2db9349ace308
KirariXSaotame <virsus819@gmail.com>
2023-03-16 13:03:53 +0700

    ознакомились с роутером

--- src/Pages/Route/RouteComponent.tsx
+++ src/Pages/Route/RouteComponent.tsx
@@ -6,6 +6,9 @@ import {
 import MainPage from "../MainPage";
 import StudentsPage from "../StudentsPage";
 import KryuchkovNickPage from "../StudentPage/KryuchkovNick/KryuchkovNickPage";
+import ProkhorovMihailPage from "../StudentPage/ProkhorovMihail/ProkhorovMihailPage";
+import Test1Page from "../StudentPage/ProkhorovMihail/Test1/Test1Page";
+import Test2Page from "../StudentPage/ProkhorovMihail/Test2/Test2Page";
 
 const RouteComponent = () => {
     return (
@@ -15,6 +18,10 @@ const RouteComponent = () => {
                     <Route index element={<StudentsPage/>} />
 
                     <Route path={'teacher'} element={<KryuchkovNickPage/>} />
+                    <Route path={'Prokhorov'} element={<ProkhorovMihailPage/>}>
+                        <Route path={'test1'} element={<Test1Page/>}/>
+                        <Route path={'test2'} element={<Test2Page/>}/>
+                        </Route>
                 </Route>
             </Routes>
         </HashRouter>
--- src/Pages/StudentsPage.tsx
+++ src/Pages/StudentsPage.tsx
@@ -18,10 +18,10 @@ const StudentsPage = () => {
                 />
 
                 <StudentElement
-                    fio={'Крючков Николай Алексеевич'}
-                    description={'Middle fullstack developer'}
-                    imageSrc={'https://sky.pro/media/wp-content/uploads/2022/03/glavnaya-9-1.png'}
-                    navigateTo={'/teacher'}
+                    fio={'Повелитель тигровых ос'}
+                    description={'закаленный на коде'}
+                    imageSrc={'https://postila.ru/data/7e/bd/f2/ac/7ebdf2ac3f49aa924b990b19869a292db528397fed2d41d89356e6c92ec14425.jpg'}
+                    navigateTo={'/prokhorov'}
                 />
 
             </Stack>

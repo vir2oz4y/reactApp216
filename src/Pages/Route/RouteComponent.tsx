6ecebed4335b2cdc190eab5508bc9dbc2cdfe157
nicklson1 <nicklson1@yandex.ru>
2023-03-16 13:03:45 +0700

    ознакомились с роутером

--- src/Pages/Route/RouteComponent.tsx
+++ src/Pages/Route/RouteComponent.tsx
@@ -6,6 +6,8 @@ import {
 import MainPage from "../MainPage";
 import StudentsPage from "../StudentsPage";
 import KryuchkovNickPage from "../StudentPage/KryuchkovNick/KryuchkovNickPage";
+import Test1Page from "../StudentPage/KryuchkovNick/Test1/Test1Page";
+import Test2Page from "../StudentPage/KryuchkovNick/Test2/Test2Page";
 
 const RouteComponent = () => {
     return (
@@ -14,7 +16,11 @@ const RouteComponent = () => {
                 <Route path="/" element={<MainPage/>}>
                     <Route index element={<StudentsPage/>} />
 
-                    <Route path={'teacher'} element={<KryuchkovNickPage/>} />
+                    <Route path={'teacher'} element={<KryuchkovNickPage/>} >
+                        <Route path={'test1'} element={<Test1Page/>} />
+                        <Route path={'test2'} element={<Test2Page/>} />
+                    </Route>
+
                 </Route>
             </Routes>
         </HashRouter>
